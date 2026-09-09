91d39f35f04d9c444765679a275c66a2b13da5bd
albecabrera <albe.cabrera@yahoo.de>
2026-09-09 21:22:51 +0200

    feat: improve offline exercise cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,10 @@
 const CACHE='linguacode-v2';
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/','/assets/app.css','/assets/qrcode-generator.min.js','/assets/app.js','/manifest.webmanifest']))));
-self.addEventListener('fetch', event => { if (event.request.method === 'GET') event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))); });
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
+    if (response.ok && new URL(event.request.url).origin === self.location.origin) caches.open(CACHE).then(cache => cache.put(event.request, response.clone()));
+    return response;
+  }).catch(() => caches.match('/'))));
+});
