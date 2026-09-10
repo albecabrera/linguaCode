ab5fbc9dc96d053a921a6ac1f946115e164305ca
albecabrera <albe.cabrera@yahoo.de>
2026-09-10 11:25:45 +0200

    fix: refresh automata activity cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='linguacode-v4';
+const CACHE='linguacode-v5';
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/offline.html','/assets/app.css','/assets/dashboard.css','/assets/qrcode-generator.min.js','/assets/app.js','/manifest.webmanifest']))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
 self.addEventListener('fetch', event => {
